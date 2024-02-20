d90031a6ecb0fc557790484efa2c0513f56a75de
kiet.le <kiet.le@ecom365.vn>
2024-02-20 10:16:13 +0700

    setup db

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,14 +3,15 @@ import { AuthForm } from "./components/AuthForm";
 
 export default function Home() {
   return (
-    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gradient-45 from-purple-500 to-pink-500">
+    //bg-gradient-45 from-purple-500 to-pink-500
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-slate-600">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           alt="Logo"
           height={72}
           width={72}
           className="mx-auto w-auto"
-          src={"/images/logo-messenger.png"}
+          src={"/images/logo.png"}
         />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
           Welcome to Messenger
